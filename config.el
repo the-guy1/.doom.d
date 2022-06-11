0200755cd0d1b84cb3430a945b7c8e3f4dbdaa10
Kenneth Lawson <koodog@gmail.com>
2022-06-11 15:10:56 -0400

    pushing this to see how it looks on github

--- config.el
+++ config.el
@@ -1,11 +1,7 @@
 (setq beacon-mode 1) ;; enable beacon mode
-
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Kenneth Lawson"
       user-mail-address "lawsonkenneth755@gmail.com")
 
