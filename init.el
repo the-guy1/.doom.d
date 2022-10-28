f73f92dfe37410b20de24753f88ba52b54ccaf4e
Kenneth Lawson <koodog@gmail.com>
2022-10-28 17:01:47 -0400

    Theme

--- init.el
+++ init.el
@@ -162,7 +162,8 @@
        ;;plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
-       +lsp)
+       +lsp
+       +pyenv)
        ;; pip install jedi autopep8 flake8 ipython importmagic yapf
        qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
