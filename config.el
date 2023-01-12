2cb98473cdca629c12dfb50c5a17ed73b8a2da06
the-guy1 <koodog@gmail.com>
2023-01-12 01:21:54 -0500

    trying out org-agenda

--- config.el
+++ config.el
@@ -74,34 +74,34 @@
 
 (setq org-roam-directory "~/Dropbox/org/roam")
 
-;; (after! org
-  ;; (setq org-agenda-files '("~/Dropbox/org/agenda.org")))
-;;
-;; (setq
-   ;; org-fancy-priorities-list '("🟥" "🟧" "🟨")
-   ;; org-priority-faces
-   ;; '((?A :foreground "#ff6c6b" :weight bold)
-     ;; (?B :foreground "#98be65" :weight bold)
-     ;; (?C :foreground "#c678dd" :weight bold))
-   ;; org-agenda-block-separator 8411)
-;;
-;; (setq org-agenda-custom-commands
-      ;; '(("v" "A better agenda view"
-         ;; ((tags "PRIORITY=\"A\""
-                ;; ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
-                 ;; (org-agenda-overriding-header "High-priority unfinished tasks:")))
-          ;; (tags "PRIORITY=\"B\""
-                ;; ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
-                 ;; (org-agenda-overriding-header "Medium-priority unfinished tasks:")))
-          ;; (tags "PRIORITY=\"C\""
-                ;; ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
-                 ;; (org-agenda-overriding-header "Low-priority unfinished tasks:")))
-          ;; (tags "customtag"
-                ;; ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
-                 ;; (org-agenda-overriding-header "Tasks marked with customtag:")))
-;;
-          ;; (agenda "")
-          ;; (alltodo "")))))
+(after! org
+  (setq org-agenda-files '("~/Dropbox/org/agenda.org")))
+
+(setq
+   org-fancy-priorities-list '("🟥" "🟧" "🟨")
+   org-priority-faces
+   '((?A :foreground "#ff6c6b" :weight bold)
+     (?B :foreground "#98be65" :weight bold)
+     (?C :foreground "#c678dd" :weight bold))
+   org-agenda-block-separator 8411)
+
+(setq org-agenda-custom-commands
+      '(("v" "A better agenda view"
+         ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "High-priority unfinished tasks:")))
+          (tags "PRIORITY=\"B\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "Medium-priority unfinished tasks:")))
+          (tags "PRIORITY=\"C\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "Low-priority unfinished tasks:")))
+          (tags "customtag"
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "Tasks marked with customtag:")))
+
+          (agenda "")
+          (alltodo "")))))
 
 (setq org-latex-pdf-process
       '("latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
